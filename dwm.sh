9cabfb830cca732d602efc20c49fa80c1eeab0d5
Sidflea <pulgamendes@protonmail.com>
2026-01-02 01:23:30 +0000

    Update dwm.sh

--- dwm.sh
+++ dwm.sh
@@ -1,32 +1,24 @@
 #!/bin/bash
 
-echo "==> Actualização do sistema"
-sudo apt update
-sudo apt upgrade -y
+echo "Instalar dependências para dwm"
+sudo apt install build-essential libx11-dev libxinerama-dev sharutils suckless-tools libxft-dev stterm
+sudo apt install git make gcc vim wget curl
 
-echo "==> Instalação de dependências essenciais"
-sudo apt install xorg xserver-xorg-core xserver-xorg-input-all xserver-xorg-video-all build-essential git libx11-dev libxinerama-dev libxft-dev libxcb1-dev libxrandr-dev libxinerama1 libxft2 libfontconfig1 fonts-dejavu dbus-x11 xinit picom 
-
-echo "==> Criar directório suckless"
-mkdir -p ~/suckless
-cd ~/suckless
-
-echo "==> Clone dwm"
+echo "Git clone de Dwm, Dmenu, st"
+makdir suckless
+cd suckless
 git clone https://git.suckless.org/dwm
-
-echo "==> Clone st"
-git clone https://git.suckless.org/st
-
-echo "==> Clone dmenu"
 git clone https://git.suckless.org/dmenu
-
-echo "==> Make dwm/st/dmenu"
-cd dwm 
-sudo make clean install -y
-cd ..
-cd st
-sudo make clean install -y
-cd ..
-cd dmenu
-sudo make clean install -y
-
+git clone https://git.suckless.org/st
+echo "dwm instalar"
+cd dwm
+make
+sudo make install
+echo "dmenu instalar"
+cd ../dmenu
+make
+sudo make install
+echo "st instalar"
+cd ../st
+make 
+sudo make install
